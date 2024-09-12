4fd690ce760b36fd733ecd584a0071d846995985
Samara Bastos <samarabastos.dev@gmail.com>
2024-09-12 16:13:09 -0300

    Commit inicial

---
+++ src/test/java/com/db/thebookclub/ThebookclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.thebookclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebookclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
