49dddb22b64208aa5e1e71ea2a1a1600f276dcdf
Samara Bastos <samarabastos.dev@gmail.com>
2024-09-13 17:10:10 -0300

    Configuração da entidade autor

---
+++ src/main/java/com/db/thebookclub/model/Autor.java
@@ -0,0 +1,37 @@
+package com.db.thebookclub.model;
+
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private LocalDate nascimento;
+
+    @Column(nullable = false,unique = true)
+    private String cpf;
+
+    @Column
+    private String genero;
+
+}
