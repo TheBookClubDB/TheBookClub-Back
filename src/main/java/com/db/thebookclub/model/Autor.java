0d3f53ab584e3cb7c0c156ba92889a3700b6811b
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-16 15:21:50 -0300

    refactor: Mudanças nos atributos de Autor

--- src/main/java/com/db/thebookclub/model/Autor.java
+++ src/main/java/com/db/thebookclub/model/Autor.java
@@ -1,18 +1,23 @@
 package com.db.thebookclub.model;
 
 import java.time.LocalDate;
+
+import com.db.thebookclub.enums.Genero;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -22,7 +27,7 @@ public class Autor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 70)
     private String nome;
 
     @Column(nullable = false)
@@ -31,7 +36,6 @@ public class Autor {
     @Column(nullable = false,unique = true)
     private String cpf;
 
-    @Column
-    private String genero;
+    private Genero genero;
 
 }
