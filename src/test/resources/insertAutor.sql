52353124fb42eabdc5659eadc1e70d815286ab9a
Samara Bastos <samarabastos.dev@gmail.com>
2024-09-23 20:03:29 -0300

    test: configuração do banco de teste

---
+++ src/test/resources/insertAutor.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.autor (id, cpf, genero, nascimento, nome)
+VALUES 
+(1, '513.099.230-00', 'MASCULINO', '1990-05-10', 'João Silva'),
+(2, '987.654.321-00', 'FEMININO', '1990-03-15', 'Ana Souza'),
+(3, '555.444.333-11', 'OUTRO', '2000-12-05', 'Alex Santos');
