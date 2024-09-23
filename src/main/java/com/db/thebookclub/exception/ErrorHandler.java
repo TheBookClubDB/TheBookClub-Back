3423c012967225fed5649bf9d92e32480d334c29
Samara Bastos <samarabastos.dev@gmail.com>
2024-09-23 19:57:39 -0300

    feat: Tratativa da exception de autor cadastrado

---
+++ src/main/java/com/db/thebookclub/exception/ErrorHandler.java
@@ -0,0 +1,15 @@
+package com.db.thebookclub.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    
+    @ExceptionHandler(AutorJaCadastradoException.class)
+    public ResponseEntity<String> handleErrorAutorJaCadastradoException(AutorJaCadastradoException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
