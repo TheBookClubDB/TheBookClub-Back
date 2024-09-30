95a4e180810a54f8c9d32403e0583260356eefdb
loopfagundes <lof.fagundes@gmail.com>
2024-09-30 11:58:19 -0300

    adicionei os documentos no projeto.

--- src/main/java/TheBookClub/framework/utils/CreateFolder.java
+++ src/main/java/TheBookClub/framework/utils/CreateFolder.java
@@ -11,5 +11,4 @@ public class CreateFolder {
             System.out.println("A pasta foi criada!");
         }
     }
-
 }
--- src/main/java/TheBookClub/framework/webDrivers/DriverManager.java
+++ src/main/java/TheBookClub/framework/webDrivers/DriverManager.java
@@ -19,5 +19,4 @@ public class DriverManager {
             getDriver().quit();
         }
     }
-
 }
