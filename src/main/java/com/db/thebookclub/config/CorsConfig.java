2dc2d99f5ea0a50bcefed651015afa45217afbd2
Samara Bastos <samarabastos.dev@gmail.com>
2024-10-16 10:08:56 -0300

    feat: Configura o cross origin

---
+++ src/main/java/com/db/thebookclub/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.db.thebookclub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registro) {
+                registro.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173","https://the-book-club.onrender.com") 
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
