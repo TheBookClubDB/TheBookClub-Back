1ee580e71f9a8ff61d120ed021bd9a5d9d307b50
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-10-18 08:52:49 -0300

    Ajustando eimports da controller

--- src/main/java/com/db/thebookclub/controller/AutorController.java
+++ src/main/java/com/db/thebookclub/controller/AutorController.java
@@ -3,11 +3,11 @@ package com.db.thebookclub.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import com.db.thebookclub.dto.autor.AutorRequest;
 import com.db.thebookclub.dto.autor.AutorResponse;
 import com.db.thebookclub.service.autor.AutorService;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
