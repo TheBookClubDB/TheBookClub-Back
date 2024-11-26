682c4b329305b562c077885e968ebe0001b40601
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-11-26 17:00:28 -0300

    feat/ reo retirado codigo  da contrler,que não faz parte da task

--- src/main/java/com/db/thebookclub/controller/AutorController.java
+++ src/main/java/com/db/thebookclub/controller/AutorController.java
@@ -1,6 +1,5 @@
 package com.db.thebookclub.controller;
 
-import com.db.thebookclub.dto.autor.AutorAtualizado;
 import com.db.thebookclub.dto.autor.AutorRequest;
 import com.db.thebookclub.dto.autor.AutorResponse;
 import com.db.thebookclub.service.autor.AutorService;
@@ -12,11 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/autor")
@@ -29,15 +23,4 @@ public class AutorController {
     public ResponseEntity<AutorResponse> registrar(@RequestBody @Valid AutorRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.registrar(request));
     }
-
-    @GetMapping()
-    public ResponseEntity<List<AutorResponse>> listar() {
-        return ResponseEntity.status(HttpStatus.OK).body(service.listar(""));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<AutorResponse> atualizar(@PathVariable Long id, @RequestBody AutorAtualizado atualizacao) {
-        return  ResponseEntity.status(HttpStatus.OK).body(service.atualizarAutorPorId(id, atualizacao));
-    }
-
 }
