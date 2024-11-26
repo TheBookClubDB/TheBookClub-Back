01bb8f0a812128f53a126739eaa3632d03ab2bf6
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-11-26 15:09:46 -0300

    feat/ reorganização de imports e retirada de espaços desnecessários, para rodar a pipe line

--- src/test/java/com/db/thebookclub/unitarios/AutorTest.java
+++ src/test/java/com/db/thebookclub/unitarios/AutorTest.java
@@ -1,39 +1,40 @@
 package com.db.thebookclub.unitarios;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+import com.db.thebookclub.dto.autor.AutorAtualizado;
+import com.db.thebookclub.dto.autor.AutorRequest;
+import com.db.thebookclub.dto.autor.AutorResponse;
+import com.db.thebookclub.enums.Genero;
+import com.db.thebookclub.exception.AutorJaCadastradoException;
+import com.db.thebookclub.exception.AutorNaoEncontradoException;
+import com.db.thebookclub.fixture.AutorFixture;
+import com.db.thebookclub.mapper.AutorMapper;
+import com.db.thebookclub.model.Autor;
+import com.db.thebookclub.repository.AutorRepository;
+import com.db.thebookclub.service.autor.AutorServiceImpl;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import com.db.thebookclub.dto.autor.AutorAtualizado;
-import com.db.thebookclub.enums.Genero;
-import com.db.thebookclub.exception.AutorNaoEncontradoException;
-import com.db.thebookclub.mapper.AutorMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import com.db.thebookclub.dto.autor.AutorRequest;
-import com.db.thebookclub.dto.autor.AutorResponse;
-import com.db.thebookclub.exception.AutorJaCadastradoException;
-import com.db.thebookclub.fixture.AutorFixture;
-import com.db.thebookclub.model.Autor;
-import com.db.thebookclub.repository.AutorRepository;
-import com.db.thebookclub.service.autor.AutorServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AutorTest {
@@ -193,7 +194,7 @@ public class AutorTest {
 
     @Test
     @DisplayName("Deve atualizar o autor")
-    void deveAtualizarOAutor() {
+    void deveAtualizarOutor() {
         autor = autor.builder()
                 .id(1L)
                 .nome(requestValido.nome())
@@ -204,13 +205,13 @@ public class AutorTest {
         BDDMockito.given(repository.findById(1L)).willReturn(Optional.ofNullable(autor));
 
         service.atualizarAutorPorId(1L, atualizacao);
-        maper.atualizarAutor(autor,atualizacao);
-        BDDMockito.then(maper).should().atualizarAutor(autor,atualizacao);
+        maper.atualizarAutor(autor, atualizacao);
+        BDDMockito.then(maper).should().atualizarAutor(autor, atualizacao);
         BDDMockito.then(repository).should().save(autorCaptor.capture());
         Autor autorAtualizado = autorCaptor.getValue();
 
-        Assertions.assertEquals("novoNome",autorAtualizado.getNome());
-        Assertions.assertEquals("2024-03-11",autorAtualizado.getNascimento().toString());
-        Assertions.assertEquals("FEMININO",autorAtualizado.getGenero().toString());
+        Assertions.assertEquals("novoNome", autorAtualizado.getNome());
+        Assertions.assertEquals("2024-03-11", autorAtualizado.getNascimento().toString());
+        Assertions.assertEquals("FEMININO", autorAtualizado.getGenero().toString());
     }
 }
